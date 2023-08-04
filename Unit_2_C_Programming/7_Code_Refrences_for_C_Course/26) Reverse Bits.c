33b082450a8a586db5c60b4ed013de3d383c2c26
MohamedMagdyJarrah <mohamedmagdygarrah@gmail.com>
2023-08-04 03:18:25 +0300

    Add Code

--- Unit_2_C_Programming/7_Code_Refrences_for_C_Course/26) Reverse Bits.c
+++ Unit_2_C_Programming/7_Code_Refrences_for_C_Course/26) Reverse Bits.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int ReverseBits(unsigned char num)
+unsigned char ReverseBits(unsigned char num)
 {
 	unsigned char reversedNum = 0 , i=0;
 	for(i=0 ; i < 8 ; i++)
